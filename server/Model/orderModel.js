8384808caa1d7da4aa8c38c5f41ead9ca1863327
Loyalty <lohunmule@gmail.com>
2024-09-23 05:39:57 -0700

    changes

--- server/Model/orderModel.js
+++ server/Model/orderModel.js
@@ -35,6 +35,10 @@ const orderSchema = new mongoose.Schema({
         type: Number,
         required: true,
       },
+    channel: {
+        type: String,
+        required: true,
+      },
     createdAt: {
         type: Date,
         default: Date.now,
