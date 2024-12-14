c022ab25aa0aa8d35bf3fdd7287cb6d3901ced1c
Loyalty <lohunmule@gmail.com>
2024-12-14 14:53:44 -0800

    changes

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(
 
 const createSecretToken = ({_id, firstName}) => {  
   return jwt.sign({_id, firstName}, process.env.TOKEN_KEY, { 
-    expiresIn: '24h',
+    expiresIn: '10s',
   });
 }
 
